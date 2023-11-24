aa728eb43e20d4c6a1e4ae9c1a857a86fc761379
NichiHachi <kevin.angelier@aol.fr>
2023-11-24 22:46:26 +0100

    Parse LeetCode API and select a random problem

--- grindset.js
+++ grindset.js
@@ -1,9 +1,11 @@
 const currentUrl = window.location.href;
 
-
 checkNSFW(currentUrl).then(result => {
     if(result){
         console.log("So you choose death.");
+        parseLeetCode().then(problemSelected => {
+            window.location.href = "https://leetcode.com/problems/" + problemSelected.stat.question__title_slug;
+        });
     }
     else{
         console.log("The website is safe :)");
@@ -16,12 +18,12 @@ checkNSFW(currentUrl).then(result => {
 * @returns {boolean} - Return true if it has a lot of nsfw words or a big no no word.
 */ 
 async function checkNSFW(URL) {
-    const nsfwWordsBigNoNo = ['xxx','porn'];
-    const nsfwWords = ['hentai'];
+    const nsfwWordsBigNoNo = ['adult-oriented','age-restricted'];
+    const nsfwWords = ['hentai','xxx','porn','sex','pussy','nude'];
     nsfwWords.push(...nsfwWordsBigNoNo);
 
     if(nsfwWords.some(word => URL.toLowerCase().includes(word))){
-        console.log("The URL is a bit sussy !")
+        console.log("The URL is a bit sussy !");
         return true;
     }
     
@@ -34,7 +36,8 @@ async function checkNSFW(URL) {
         }
         sum += correspondence[word];
     }
-    return sum > 10;
+
+    return sum > 20;
 }
 
 /**
@@ -48,23 +51,19 @@ async function parseAndAnalyze(URL, nsfwWords){
 
     // If the page contains NSFW words
     if(!URL.includes("google.com/")){
-        try {
+        try{
             const response = await fetch(URL, { method: 'GET', mode: 'cors' });
             const html = await response.text();
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
             
-            const elements = doc.querySelectorAll('*');
-            elements.forEach(element => {
-                nsfwWords.forEach(word => {
-                    if (element.textContent.toLowerCase().includes(word)) {
-                        if (!correspondence[word]) {
-                            correspondence[word] = 1;
-                        } else {
-                            correspondence[word] += 1;
-                        }
-                    }
-                });
+            const textContent = doc.body.innerText.toLowerCase();
+
+            nsfwWords.forEach(word => {
+                const wordCount = (textContent.match(new RegExp(word, 'g')) || []).length;
+                if (wordCount > 0) {
+                    correspondence[word] = wordCount;
+                }
             });
 
             for (const word in correspondence) {
@@ -72,12 +71,35 @@ async function parseAndAnalyze(URL, nsfwWords){
                     console.log(`'${word}': fetched ${correspondence[word]} times`);
                 }
             }
-        } catch (error) {
-            console.error('Error when loading the page :', error);
+        } 
+        catch (error){
+            console.error('Error when loading the page:', error);
         }
-    } else {
+    } 
+    else{
         console.log('This is a Google search page !');
     }
 
     return correspondence;
+}
+
+/**
+ * This function parses the LeetCode API and returns a random problem.
+ * @returns {object} - Return a random problem.
+ */
+async function parseLeetCode() {
+    const leetCodeAPI = "https://leetcode.com/api/problems/all/";
+    try {
+        const response = await fetch(leetCodeAPI, { method: 'GET', mode: 'cors' });
+        const data = await response.json();
+
+        // Take only the free problems
+        const listProblems = data.stat_status_pairs.filter(item => !item.paid_only);
+
+        // Select and return a random problem
+        return listProblems[Math.floor(Math.random() * listProblems.length)+1];
+    } 
+    catch (error){
+        console.log('Error when loading the page:', error);
+    }
 }
