91a97c830aa0bbb4562f879cb187cb1314d7e652
NichiHachi <kevin.angelier@aol.fr>
2023-11-25 21:54:05 +0100

    Add a popup and personalization

--- grindset.js
+++ grindset.js
@@ -1,86 +1,139 @@
 const currentUrl = window.location.href;
+var data = {};
 
-checkNSFW(currentUrl).then(result => {
-    if(result){
-        console.log("So you choose death.");
-        parseLeetCode().then(problemSelected => {
-            window.location.href = "https://leetcode.com/problems/" + problemSelected.stat.question__title_slug;
+if(!!chrome){
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        chrome.storage.local.set(request, function () {
+            console.log(request);
         });
+        sendResponse({ result: "Data recieved!" });
+    });
+}
+else if(!!browser){
+    browser.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            browser.storage.local.set(request, function () {
+                console.log(request);
+            });
+            sendResponse({ result: "Data recieved!" });
+        });
+
     }
-    else{
-        console.log("The website is safe :)");
-    }
+else{
+    console.log("The navigator is not supported");
+}
+
+if(!!chrome){
+chrome.storage.local.get(null, function (data) {
+    start(data);
 });
+}
+else if(!!browser){
+    browser.storage.local.get(null, function (data) {
+        start(data);
+    });
+}
+else{
+    console.log("The navigator is not supported");
+}
+
+
+function start(data){
+    checkWebsite(currentUrl,data).then(result => {
+        if (result) {
+            // More like me when I did the data transfer...
+            console.log("So you choose death.");
+            if(data.redirectURL===""){
+            parseLeetCode().then(problemSelected => {
+                window.location.href = "https://leetcode.com/problems/" + problemSelected.stat.question__title_slug;
+            });
+            }
+            else{
+                if(data.redirectURL.includes("http")){
+                    window.location.href = data.redirectURL;
+                }
+                else{
+                    window.location.href = "http://"+data.redirectURL;
+                }
+            }
+        }
+        else {
+            console.log("The website is safe :)");
+        }
+    });
+}
 
 /**
 * This function checks if the URL is a nsfw website.
 * @param {string} URL - The URL to check.
 * @returns {boolean} - Return true if it has a lot of nsfw words or a big no no word.
-*/ 
-async function checkNSFW(URL) {
-    const nsfwWordsBigNoNo = ['adult-oriented','age-restricted'];
-    const nsfwWords = ['hentai','xxx','porn','sex','pussy','nude'];
-    nsfwWords.push(...nsfwWordsBigNoNo);
-
-    if(nsfwWords.some(word => URL.toLowerCase().includes(word))){
-        console.log("The URL is a bit sussy !");
-        return true;
+*/
+async function checkWebsite(URL,data) {
+    var nsfwWordsBigNoNo = [];
+    var blackListWords = [];
+    const dataBlackList = data.blackList.split(/[ ,\n]+/).filter(Boolean);
+    if(data.blackListOnly!==true){
+        nsfwWordsBigNoNo = ['adult-oriented', 'age-restricted'];
+        blackListWords = ['hentai', 'xxx', 'porn', 'sex', 'pussy', 'nude'];
+        blackListWords.push(...dataBlackList);
     }
-    
-    const correspondence = await parseAndAnalyze(URL,nsfwWords);
-
-    let sum = 0;
-    for (const word in correspondence){
-        if(nsfwWordsBigNoNo.includes(word)){
-            return true;
-        }
-        sum += correspondence[word];
+    else{
+        blackListWords = dataBlackList;
     }
 
-    return sum > 20;
+    return parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data);
 }
 
 /**
  * This function parses the URL and checks the nsfw words in it. 
  * @param {string} URL - The URL to parse.
- * @param {[string]} nsfwWords - The list of word to check.
+ * @param {[string]} blackListWords - The list of word to check.
  * @returns {object} - Return a hash map of the number of nsfw word.
  */
-async function parseAndAnalyze(URL, nsfwWords){
-    const correspondence = {};
-
+async function parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data) {
+    const whiteListedWebsites = ['google.com/', 'leetcode.com/'];
+    whiteListedWebsites.push(...data.whiteList.split(/[ ,\n]+/).filter(Boolean));
     // If the page contains NSFW words
-    if(!URL.includes("google.com/")){
-        try{
+    if (!whiteListedWebsites.some(website => URL.toLowerCase().includes(website))) {
+        try {
             const response = await fetch(URL, { method: 'GET', mode: 'cors' });
             const html = await response.text();
             const parser = new DOMParser();
             const doc = parser.parseFromString(html, 'text/html');
-            
+
             const textContent = doc.body.innerText.toLowerCase();
 
-            nsfwWords.forEach(word => {
-                const wordCount = (textContent.match(new RegExp(word, 'g')) || []).length;
-                if (wordCount > 0) {
-                    correspondence[word] = wordCount;
+            if (blackListWords.some(word => URL.toLowerCase().includes(word))) {
+                console.log("The URL is a bit sussy !");
+                return true;
+            }
+
+            if (nsfwWordsBigNoNo.some(word => textContent.includes(word))) {
+                return true;
+            }
+
+            let wordCount = 0;
+            blackListWords.forEach(word => {
+                wordCount += (textContent.match(new RegExp(word, 'g')) || []).length;
+                // If the word count is greater than the sensitivity, that's a big no no!
+                // Function : (x/5)^2+x+1
+                if (wordCount >= (data.sensitivity/5)**2+data.sensitivity+1) {
+                    return true;
                 }
             });
 
-            for (const word in correspondence) {
-                if (correspondence.hasOwnProperty(word)) {
-                    console.log(`'${word}': fetched ${correspondence[word]} times`);
-                }
-            }
-        } 
-        catch (error){
+            return false;
+        }
+        catch (error) {
             console.error('Error when loading the page:', error);
         }
-    } 
-    else{
-        console.log('This is a Google search page !');
+    }
+    else {
+        console.log('This is a WhiteListed website !');
     }
 
-    return correspondence;
+    return false;
 }
 
 /**
@@ -88,7 +141,13 @@ async function parseAndAnalyze(URL, nsfwWords){
  * @returns {object} - Return a random problem.
  */
 async function parseLeetCode() {
-    const leetCodeAPI = "https://leetcode.com/api/problems/all/";
+    var leetCodeAPI;
+    if(typeof browser !== "undefined"){
+        leetCodeAPI = "https://leetcode.com/api/problems/all/";
+    }
+    else if(typeof chrome !== "undefined"){
+        leetCodeAPI =  "https://cors-anywhere.herokuapp.com/" + "https://leetcode.com/api/problems/all/";
+    }
     try {
         const response = await fetch(leetCodeAPI, { method: 'GET', mode: 'cors' });
         const data = await response.json();
@@ -97,9 +156,9 @@ async function parseLeetCode() {
         const listProblems = data.stat_status_pairs.filter(item => !item.paid_only);
 
         // Select and return a random problem
-        return listProblems[Math.floor(Math.random() * listProblems.length)+1];
-    } 
-    catch (error){
+        return listProblems[Math.floor(Math.random() * listProblems.length) + 1];
+    }
+    catch (error) {
         console.log('Error when loading the page:', error);
     }
 }
