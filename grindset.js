b46bfb30770c9d98fb8821a16f59a546b52d8b53
NichiHachi <kevin.angelier@aol.fr>
2023-11-28 23:37:58 +0100

    Cleaner code and speech bubble change

--- grindset.js
+++ grindset.js
@@ -1,60 +1,75 @@
 const currentUrl = window.location.href;
-var data = {};
 
-if(!!chrome){
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        chrome.storage.local.set(request, function () {
-            console.log(request);
-        });
-        sendResponse({ result: "Data recieved!" });
+const defaultChoices = {
+    sensitivity: 25,
+    blackListOnly: false,
+    blackList: "",
+    whiteList: "",
+    redirectURL: ""
+}
+
+// If the navigator is Chrome, get the data stored in the google local storage and start the extension.
+if (!!chrome) {
+    chrome.storage.local.get(null, function (dataUserChoices) {
+        start(Object.keys(dataUserChoices).length === 0 ? defaultChoices : dataUserChoices);
     });
 }
-else if(!!browser){
+// If the navigator is Firefox, get the data stored in the firefox local storage and start the extension.
+else if (!!browser) {
+    browser.storage.local.get(null, function (dataUserChoices) {
+        start(Object.keys(dataUserChoices).length === 0 ? defaultChoices : dataUserChoices);
+    });
+}
+else {
+    console.log("The navigator is not supported");
+}
+
+// Get the userDataChoices from the popup
+if (!!chrome) {
+    chrome.runtime.onMessage.addListener(
+        function (request, sendResponse) {
+            chrome.storage.local.set(request, function () {
+                console.log(request);
+            });
+            sendResponse({ result: "Data received!" });
+        });
+}
+else if (!!browser) {
     browser.runtime.onMessage.addListener(
-        function (request, sender, sendResponse) {
+        function (request, sendResponse) {
             browser.storage.local.set(request, function () {
                 console.log(request);
             });
-            sendResponse({ result: "Data recieved!" });
+            sendResponse({ result: "Data received!" });
         });
-
-    }
-else{
+}
+else {
     console.log("The navigator is not supported");
 }
 
-if(!!chrome){
-chrome.storage.local.get(null, function (data) {
-    start(data);
-});
-}
-else if(!!browser){
-    browser.storage.local.get(null, function (data) {
-        start(data);
-    });
-}
-else{
-    console.log("The navigator is not supported");
-}
-
-
-function start(data){
-    checkWebsite(currentUrl,data).then(result => {
+/**
+ * This function starts the extension.
+ * @param {object} dataUserChoices The user choices.
+ */
+function start(dataUserChoices) {
+    checkWebsite(currentUrl, dataUserChoices).then(result => {
         if (result) {
-            // More like me when I did the data transfer...
+            // More like me when I did the dataUserChoices transfer...
             console.log("So you choose death.");
-            if(data.redirectURL===""){
-            parseLeetCode().then(problemSelected => {
-                window.location.href = "https://leetcode.com/problems/" + problemSelected.stat.question__title_slug;
-            });
+            // If the user didn't enter an URL to be redirected, we redirect him to a random LeetCode problem.
+            if (dataUserChoices.redirectURL === "") {
+                parseLeetCode().then(problemSelected => {
+                    window.location.href = "https://leetcode.com/problems/" + problemSelected.stat.question__title_slug;
+                });
             }
-            else{
-                if(data.redirectURL.includes("http")){
-                    window.location.href = data.redirectURL;
+            // Else we redirect him to the URL he entered.
+            else {
+                // Verify if the URL contains http or https (Otherwise the user can't be redirected).
+                if (dataUserChoices.redirectURL.includes("http")) {
+                    window.location.href = dataUserChoices.redirectURL;
                 }
-                else{
-                    window.location.href = "http://"+data.redirectURL;
+                else {
+                    window.location.href = "http://" + dataUserChoices.redirectURL;
                 }
             }
         }
@@ -65,36 +80,33 @@ function start(data){
 }
 
 /**
-* This function checks if the URL is a nsfw website.
-* @param {string} URL - The URL to check.
-* @returns {boolean} - Return true if it has a lot of nsfw words or a big no no word.
+* This function checks if the URL is safe to go.
+* @param {string} URL The URL to check.
+* @param {object} dataUserChoices The user choices.
+* @returns {boolean} Return true if the URL or the website countain the blacklisted words.
 */
-async function checkWebsite(URL,data) {
-    var nsfwWordsBigNoNo = [];
-    var blackListWords = [];
-    const dataBlackList = data.blackList.split(/[ ,\n]+/).filter(Boolean);
-    if(data.blackListOnly!==true){
-        nsfwWordsBigNoNo = ['adult-oriented', 'age-restricted'];
-        blackListWords = ['hentai', 'xxx', 'porn', 'sex', 'pussy', 'nude'];
-        blackListWords.push(...dataBlackList);
-    }
-    else{
-        blackListWords = dataBlackList;
-    }
+async function checkWebsite(URL, dataUserChoices) {
+    const userBlackList = dataUserChoices.blackList.split(/[ ,\n]+/).filter(Boolean);
 
-    return parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data);
+    // If the user didn't check the "Blacklist only" checkbox, we add his list to the default blacklisted words
+    const blackList = dataUserChoices.blackListOnly ? userBlackList : ['hentai', 'xxx', 'porn', 'sex', 'pussy', 'nude', ...userBlackList];
+    const nsfwWordsBigNoNo = dataUserChoices.blackListOnly ? [] : ['adult-oriented', 'age-restricted'];
+
+    return parseAndAnalyze(URL, blackList, nsfwWordsBigNoNo, dataUserChoices);
 }
 
 /**
- * This function parses the URL and checks the nsfw words in it. 
+ * This function parses the URL and checks the blacklist words is in it.
  * @param {string} URL - The URL to parse.
- * @param {[string]} blackListWords - The list of word to check.
- * @returns {object} - Return a hash map of the number of nsfw word.
+ * @param {[string]} blackList - The list of word to check.
+ * @param {[string]} nsfwWordsBigNoNo - The list of word to check.
+ * @param {object} dataUserChoices - The user choices.
+ * @returns {boolean} - Return true if the website countain more then the userChoices blacklist words. (By default it's >= 25)
  */
-async function parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data) {
-    const whiteListedWebsites = ['google.com/', 'leetcode.com/'];
-    whiteListedWebsites.push(...data.whiteList.split(/[ ,\n]+/).filter(Boolean));
-    // If the page contains NSFW words
+async function parseAndAnalyze(URL, blackList, nsfwWordsBigNoNo, dataUserChoices) {
+    const whiteListedWebsites = ['google.com/', 'leetcode.com/', ...dataUserChoices.whiteList.split(/[ ,\n]+/).filter(Boolean)];
+
+    // If the page is a whitelisted website, we don't need to check it.
     if (!whiteListedWebsites.some(website => URL.toLowerCase().includes(website))) {
         try {
             const response = await fetch(URL, { method: 'GET', mode: 'cors' });
@@ -104,24 +116,24 @@ async function parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data) {
 
             const textContent = doc.body.innerText.toLowerCase();
 
-            if (blackListWords.some(word => URL.toLowerCase().includes(word))) {
+            // If the URL contains a blacklisted word, that's a big no no!
+            if (blackList.some(word => URL.toLowerCase().includes(word))) {
                 console.log("The URL is a bit sussy !");
                 return true;
             }
 
+            // If the website contains a certain blacklisted word, that's a big no no!
             if (nsfwWordsBigNoNo.some(word => textContent.includes(word))) {
                 return true;
             }
 
-            let wordCount = 0;
-            blackListWords.forEach(word => {
-                wordCount += (textContent.match(new RegExp(word, 'g')) || []).length;
-                // If the word count is greater than the sensitivity, that's a big no no!
-                // Function : (x/5)^2+x+1
-                if (wordCount >= (data.sensitivity/5)**2+data.sensitivity+1) {
-                    return true;
-                }
-            });
+            // Check if the website contains more than the detection sensitivity.
+            const countOccurrences = (text, regex) => (text.match(regex) || []).length;
+            let wordCount = blackList.reduce((count, word) => count + countOccurrences(textContent, new RegExp(word, 'g')), 0);
+            // Function : (x/5)^2+x+1
+            if (wordCount >= (dataUserChoices.sensitivity / 5) ** 2 + dataUserChoices.sensitivity + 1) {
+                return true;
+            }
 
             return false;
         }
@@ -142,18 +154,20 @@ async function parseAndAnalyze(URL, blackListWords,nsfwWordsBigNoNo,data) {
  */
 async function parseLeetCode() {
     var leetCodeAPI;
-    if(typeof browser !== "undefined"){
+    // If the navigator is Firefox, we don't need to use a proxy.
+    if (typeof browser !== "undefined") {
         leetCodeAPI = "https://leetcode.com/api/problems/all/";
     }
-    else if(typeof chrome !== "undefined"){
-        leetCodeAPI =  "https://cors-anywhere.herokuapp.com/" + "https://leetcode.com/api/problems/all/";
+    // If the navigator is Chrome, we need to use a proxy.
+    else if (typeof chrome !== "undefined") {
+        leetCodeAPI = "https://cors-anywhere.herokuapp.com/" + "https://leetcode.com/api/problems/all/";
     }
     try {
         const response = await fetch(leetCodeAPI, { method: 'GET', mode: 'cors' });
-        const data = await response.json();
+        const dataUserChoices = await response.json();
 
-        // Take only the free problems
-        const listProblems = data.stat_status_pairs.filter(item => !item.paid_only);
+        // Take only the free problems (May change in the future)
+        const listProblems = dataUserChoices.stat_status_pairs.filter(item => !item.paid_only);
 
         // Select and return a random problem
         return listProblems[Math.floor(Math.random() * listProblems.length) + 1];
--- popup.js
+++ popup.js
@@ -1,74 +1,82 @@
 window.onload = function () {
-  var slider = document.getElementById("sensitivity");
-  var chadWisdom = document.getElementById("chadWisdom");
-  var blackListOnly = document.getElementById("blackListOnly");
-  var blackList = document.getElementById("blackList");
-  var whiteList = document.getElementById("whiteList");
-  var redirectURL = document.getElementById("redirectURL");
+  // Get the elements from the popup.html file.
+  const elements = [
+    { id: 'sensitivity', event: 'input', action: 'value' },
+    { id: 'blackListOnly', event: 'click', action: 'checked' },
+    { id: 'blackList', event: 'change', action: 'value' },
+    { id: 'whiteList', event: 'change', action: 'value' },
+    { id: 'redirectURL', event: 'change', action: 'value' },
+  ];
 
-  slider.value = localStorage.getItem('sensitivity') || slider.value;
-  blackListOnly.checked = localStorage.getItem('blackListOnly') == 'true' || blackListOnly.checked;
-  blackList.value = localStorage.getItem('blackList') || blackList.value;
-  whiteList.value = localStorage.getItem('whiteList') || whiteList.value;
-  redirectURL.value = localStorage.getItem('redirectURL') || redirectURL.value;
+  // Get the values from the local storage. If no value is stored, we use the default value.
+  elements.forEach(({ id, event, action }) => {
+    const element = document.getElementById(id);
+    element[action] = localStorage.getItem(id) || element[action];
 
-  sensitivityTextChange(slider.value);
-  chadWisdom.innerHTML = listOfWisdom[Math.floor(Math.random() * listOfWisdom.length)];
-
-  slider.oninput = function () {
-    localStorage.setItem('sensitivity', this.value);
-    sensitivityTextChange(this.value);
-    sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL);
-  }
-
-  blackListOnly.onclick = function () {
-    localStorage.setItem('blackListOnly', this.checked);
-    sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL);
-  }
-
-  blackList.onchange = function () {
-    localStorage.setItem('blackList', this.value);
-    sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL);
-  }
-
-  whiteList.onchange = function () {
-    localStorage.setItem('whiteList', this.value);
-    sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL);
-  }
-
-
-  redirectURL.onchange = function () {
-    localStorage.setItem('redirectURL', this.value);
-    sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL);
-  }
-
-  document.querySelector('.text.oneRow').addEventListener('keydown', function(e) {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-    }
+    element.addEventListener(event, function () {
+      localStorage.setItem(id, this[action]);
+      if (element.id === 'sensitivity') {
+        sensitivityTextChange(this[action]);
+      };
+      sendInformation({
+        sensitivity: document.getElementById('sensitivity').value,
+        blackListOnly: document.getElementById('blackListOnly').checked,
+        blackList: document.getElementById('blackList').value,
+        whiteList: document.getElementById('whiteList').value,
+        redirectURL: document.getElementById('redirectURL').value
+      });
+    });
   });
-}
 
-function sendInformation(slider, blackListOnly, blackList, whiteList,redirectURL){
-  dataStorage = {
-    sensitivity: slider.value,
-    blackListOnly: blackListOnly.checked,
-    blackList: blackList.value,
-    whiteList: whiteList.value,
-    redirectURL: redirectURL.value,
-  }
+  // Print the sensitivity text.
+  sensitivityTextChange(document.getElementById('sensitivity').value);
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, dataStorage, function(response) {
+  // Print a random wisdom from the json list. 
+  fetch('wisdoms.json')
+    .then(response => response.json())
+    .then(data => {
+      chadWisdom.innerHTML = data.listOfWisdoms[Math.floor(Math.random() * data.listOfWisdoms.length)];
+    });
+
+  // Prevent the user from pressing enter in the URL redirection input (Only one URL can be enter). 
+  document.querySelector('.text.oneRow').addEventListener('keydown', (action) => {
+    if (action.key === 'Enter') action.preventDefault();
+  });
+};
+
+/**
+ * This function sends the user choices to the grindsent.js script.
+ * @param {int} sensitivity
+ * @param {boolean} blackListOnly
+ * @param {string} blackList
+ * @param {string} whiteList
+ * @param {string} redirectURL
+ */
+function sendInformation({ sensitivity, blackListOnly, blackList, whiteList, redirectURL }) {
+  const dataStorage = {
+    sensitivity,
+    blackListOnly,
+    blackList,
+    whiteList,
+    redirectURL,
+  };
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, dataStorage, function (response) {
       if (response) {
         console.log(response.result);
-      } else {
-        console.log('No response received from content script');
+      }
+      else {
+        console.log('No response received from content script!');
       }
     });
   });
-}
+};
 
+/**
+ * This function changes the text and the color of the Detection sensitivity text.
+ * @param {int} value 
+ */
 function sensitivityTextChange(value) {
   var sensitivityText = document.getElementById("sensitivityText");
   if (value == 1) {
@@ -92,42 +100,4 @@ function sensitivityTextChange(value) {
   let r = Math.floor(255 * (1 - value / 50));
   let b = Math.floor(255 * value / 50);
   sensitivityText.style.color = `rgb(${r}, 0, ${b})`;
-}
-
-const listOfWisdom = [
-  "<span style='color: #00b300;'>Seize the day</span>, champ! Time to <span style='color: #002699;'>unleash</span> that untapped <span style='color: #b30000;'>potential</span>!",
-  "Every stride <span style='color: #33004d;'>forward</span> brings you closer to <span style='color: #804000;'>greatness</span>. Keep <span style='color: #806600;'>pushing</span>!",
-  "<span style='color: #006666;'>Success</span> is not a gift, it is <span style='color: #993366;'>earned</span> one <span style='color: #cc3333;'>step</span> at a time!",
-  "<span style='color: #990000;'>Procrastination</span> delays <span style='color: #730099;'>progress</span>. Ignite it with your <span style='color: #b35900;'>determination</span>!",
-  "Fuel your <span style='color: #994d00;'>drive</span> with <span style='color: #004d00;'>ambition</span>. Nothing deters someone fueled by <span style='color: #0066cc;'>ambition</span>!",
-  "Embrace the <span style='color: #cc3300;'>challenge</span>! It's the catalyst for your <span style='color: #ff8533;'>growth</span> and <span style='color: #0066cc;'>success</span>!",
-  "You're not just aiming <span style='color: #33004d;'>high</span>, you're aiming for the <span style='color: #ff6666;'>stars</span>. Claim what's <span style='color: #b35900;'>yours</span>!",
-  "<span style='color: #00b300;'>Winners</span> never quit, and <span style='color: #33004d;'>quitters</span> never win. What's your choice?",
-  "Chase your <span style='color: #990000;'>dreams</span> passionately. They're closer than you think!",
-  "The <span style='color: #404040;'>grind</span> is not easy, but <span style='color: #666666;'>regret</span> is harder. Keep <span style='color: #000000;'>hustling</span>!",
-  "<span style='color: #b30000;'>Persistence</span> and <span style='color: #004d00;'>dedication</span> pave the road to <span style='color: #0066cc;'>victory</span>!",
-  "<span style='color: #ffcc00;'>Believe</span> in yourself, always. <span style='color: #b30000;'>Trust</span> your journey!",
-  "<span style='color: #006666;'>Today's <span style='color: #cc3333;'>efforts</span> yield tomorrow's <span style='color: #0066cc;'>rewards</span>.",
-  "No <span style='color: #990000;'>challenge</span> surpasses your <span style='color: #cc3333;'>resolve</span>.",
-  "<span style='color: #006666;'>Commit</span> to your <span style='color: #cc3300;'>goals</span>, achieve the unthinkable!",
-  "<span style='color: #404040;'>Strive</span> for progress, not perfection.",
-  "<span style='color: #004d00;'>Ambition</span> transforms <span style='color: #0066cc;'>dreams</span> into reality!",
-  "You're <span style='color: #b35900;'>stronger</span> than you think. <span style='color: #ff8533;'>Believe</span> in yourself!",
-  "<span style='color: #ff0000;'>Embrace</span> the <span style='color: #0000ff;'>unknown</span> and <span style='color: #00ff00;'>create</span> your own <span style='color: #ff00ff;'>destiny</span>!",
-  "<span style='color: #800080;'>Innovation</span> is the <span style='color: #008000;'>key</span> to <span style='color: #000080;'>unlocking</span> your <span style='color: #800000;'>potential</span>!",
-  "<span style='color: #ff8c00;'>Break</span> free from the <span style='color: #8b0000;'>ordinary</span> and <span style='color: #0000cd;'>embrace</span> the <span style='color: #8b008b;'>extraordinary</span>!",
-  "<span style='color: #808000;'>Dare</span> to <span style='color: #0000ff;'>dream</span>, <span style='color: #ff00ff;'>believe</span> in yourself, and <span style='color: #008000;'>achieve</span> greatness!",
-  "<span style='color: #ff1493;'>Think</span> outside the <span style='color: #0000cd;'>box</span> and <span style='color: #ff8c00;'>create</span> your own <span style='color: #8b008b;'>path</span>!",
-  "<span style='color: #800080;'>Don't</span> be afraid to <span style='color: #ff0000;'>fail</span>, it's a <span style='color: #0000ff;'>stepping</span> stone to <span style='color: #008000;'>success</span>!",
-  "<span style='color: #ff1493;'>Embrace</span> your <span style='color: #0000cd;'>uniqueness</span> and <span style='color: #ff8c00;'>shine</span> bright like a <span style='color: #8b008b;'>diamond</span>!",
-  "<span style='color: #808000;'>Don't</span> wait for <span style='color: #0000ff;'>opportunities</span>, <span style='color: #ff00ff;'>create</span> them!",
-  "<span style='color: #ff1493;'>Believe</span> in your <span style='color: #0000cd;'>vision</span> and <span style='color: #ff8c00;'>make</span> it a <span style='color: #8b008b;'>reality</span>!",
-  "<span style='color: #800080;'>Success</span> is the <span style='color: #008000;'>result</span> of <span style='color: #000080;'>hard</span> work and <span style='color: #800000;'>originality</span>!",
-  "<span style='color: #ff1493;'>Don't</span> be a <span style='color: #0000cd;'>follower</span>, be a <span style='color: #ff8c00;'>trailblazer</span>!",
-  "<span style='color: #808000;'>Think</span> big, <span style='color: #0000ff;'>dream</span> big, and <span style='color: #ff00ff;'>achieve</span> big!",
-  "<span style='color: #ff1493;'>Break</span> the <span style='color: #0000cd;'>rules</span> and <span style='color: #ff8c00;'>create</span> your own <span style='color: #8b008b;'>success</span> story!",
-  "<span style='color: #800080;'>Don't</span> be afraid to <span style='color: #ff0000;'>stand</span> out, be afraid of <span style='color: #0000ff;'>blending</span> in!",
-  "<span style='color: #ff1493;'>Innovate</span> or <span style='color: #0000cd;'>stagnate</span>, the choice is yours!",
-  "<span style='color: #808000;'>Don't</span> be limited by <span style='color: #0000ff;'>conformity</span>, be inspired by <span style='color: #ff00ff;'>creativity</span>!",
-  "<span style='color: #ff1493;'>Think</span> differently, <span style='color: #0000cd;'>act</span> boldly, and <span style='color: #ff8c00;'>create</span> your own <span style='color: #8b008b;'>legacy</span>!"
-];
+};
